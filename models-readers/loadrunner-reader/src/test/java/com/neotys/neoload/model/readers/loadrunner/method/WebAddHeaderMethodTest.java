b7cab097d7002bbf514a27ec3c2b51f811a6912b
Sébastien Richert <sebastien.richert@neotys.com>
2018-07-20 20:09:24 +0200

    JUnit web_add_header & web_add_auto_header

---
+++ models-readers/loadrunner-reader/src/test/java/com/neotys/neoload/model/readers/loadrunner/method/WebAddHeaderMethodTest.java
@@ -0,0 +1,39 @@
+package com.neotys.neoload.model.readers.loadrunner.method;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.neotys.neoload.model.listener.TestEventListener;
+import com.neotys.neoload.model.parsers.CPP14Parser.MethodcallContext;
+import com.neotys.neoload.model.readers.loadrunner.ImmutableMethodCall;
+import com.neotys.neoload.model.readers.loadrunner.LoadRunnerReader;
+import com.neotys.neoload.model.readers.loadrunner.LoadRunnerVUVisitor;
+import com.neotys.neoload.model.readers.loadrunner.MethodCall;
+import com.neotys.neoload.model.repository.Header;
+import com.neotys.neoload.model.repository.ImmutableHeader;
+
+public class WebAddHeaderMethodTest {
+
+	private static final LoadRunnerReader LOAD_RUNNER_READER = new LoadRunnerReader(new TestEventListener(), "", "");
+	private static final LoadRunnerVUVisitor LOAD_RUNNER_VISITOR = new LoadRunnerVUVisitor(LOAD_RUNNER_READER, "{", "}", "");
+	private static final MethodcallContext METHOD_CALL_CONTEXT = new MethodcallContext(null, 0);
+
+	public static final MethodCall WEB_ADD_HEADER = ImmutableMethodCall.builder().name("\"web_add_header\"").addParameters(
+			"\"headerName\"").addParameters("\"headerValue\"").build();
+
+	@Test
+	public void testGetElement() {
+		LOAD_RUNNER_VISITOR.getCurrentHeaders().clear();
+		LOAD_RUNNER_VISITOR.getGlobalHeaders().clear();
+		(new WebAddHeaderMethod()).getElement(LOAD_RUNNER_VISITOR, WEB_ADD_HEADER, METHOD_CALL_CONTEXT);
+		assertEquals(1, LOAD_RUNNER_VISITOR.getCurrentHeaders().size());
+		assertEquals(0, LOAD_RUNNER_VISITOR.getGlobalHeaders().size());
+		final Header actualHeader = LOAD_RUNNER_VISITOR.getCurrentHeaders().get(0);
+		final Header expectedHeader = ImmutableHeader.builder()
+				.headerName("headerName")
+				.headerValue("headerValue")
+				.build();
+		assertEquals(expectedHeader, actualHeader);
+	}
+}
