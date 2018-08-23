de9a8009f16eb467b803c0cb4666c2809bbb199f
Sébastien Richert <sebastien.richert@neotys.com>
2018-08-23 16:45:32 +0200

    Fix lr_think_time parsing + Junit

--- models-readers/loadrunner-reader/src/test/java/com/neotys/neoload/model/readers/loadrunner/method/LRThinkTimeMethodTest.java
+++ models-readers/loadrunner-reader/src/test/java/com/neotys/neoload/model/readers/loadrunner/method/LRThinkTimeMethodTest.java
@@ -9,41 +9,33 @@ import com.neotys.neoload.model.parsers.CPP14Parser.MethodcallContext;
 import com.neotys.neoload.model.readers.loadrunner.ImmutableMethodCall;
 import com.neotys.neoload.model.readers.loadrunner.LoadRunnerReader;
 import com.neotys.neoload.model.readers.loadrunner.LoadRunnerVUVisitor;
-import com.neotys.neoload.model.readers.loadrunner.MethodCall;
 import com.neotys.neoload.model.repository.Delay;
 import com.neotys.neoload.model.repository.ImmutableDelay;
 
 public class LRThinkTimeMethodTest {
-	
-	public static final MethodCall LR_THINK_TIME_1 = ImmutableMethodCall.builder()
-			.name("\"lr_think_time\"")
-			.addParameters("10")
-			.build();
-	
-	public static final MethodCall LR_THINK_TIME_2 = ImmutableMethodCall.builder()
-			.name("\"lr_think_time\"")
-			.addParameters("10.00")
-			.build();
 
 	private static final LoadRunnerReader LOAD_RUNNER_READER = new LoadRunnerReader(new TestEventListener(), "", "");
 	private static final LoadRunnerVUVisitor LOAD_RUNNER_VISITOR = new LoadRunnerVUVisitor(LOAD_RUNNER_READER, "{", "}", "");
 	private static final MethodcallContext METHOD_CALL_CONTEXT = new MethodcallContext(null, 0);
 	
 	@Test
-	public void testGetElement() {		
-		
-		Delay actualDelay = (Delay) (new LRThinkTimeMethod()).getElement(LOAD_RUNNER_VISITOR, LR_THINK_TIME_1, METHOD_CALL_CONTEXT);
-		Delay expectedDelay = ImmutableDelay.builder()
+	public void testGetElement() {
+		assertDelay("1", "1000");
+		assertDelay("10", "10000");
+		assertDelay("10.00", "10000");	
+		assertDelay("0", "0");
+		assertDelay("a", "a000");
+		assertDelay("${a}", "${a}000");
+	}
+	
+	public void assertDelay(final String lrDelay, final String modelDelay){
+		final Delay actualDelay = (Delay) (new LRThinkTimeMethod()).getElement(LOAD_RUNNER_VISITOR, ImmutableMethodCall.builder()
+				.name("\"lr_think_time\"")
+				.addParameters(lrDelay)
+				.build(), METHOD_CALL_CONTEXT);
+		final Delay expectedDelay = ImmutableDelay.builder()
 				.name("delay")
-				.delay("10000")
-				.isThinkTime(true)
-				.build();
-		assertEquals(expectedDelay, actualDelay);
-		
-		actualDelay = (Delay) (new LRThinkTimeMethod()).getElement(LOAD_RUNNER_VISITOR, LR_THINK_TIME_2, METHOD_CALL_CONTEXT);
-		expectedDelay = ImmutableDelay.builder()
-				.name("delay")
-				.delay("10000")
+				.delay(modelDelay)
 				.isThinkTime(true)
 				.build();
 		assertEquals(expectedDelay, actualDelay);
