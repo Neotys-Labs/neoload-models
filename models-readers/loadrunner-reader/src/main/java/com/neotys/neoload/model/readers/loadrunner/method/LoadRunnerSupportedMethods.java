5c1171c8469188e7a7adf886097d8925faaf549b
Sébastien Richert <sebastien.richert@neotys.com>
2018-09-05 21:03:12 +0200

    Fix sonar

--- models-readers/loadrunner-reader/src/main/java/com/neotys/neoload/model/readers/loadrunner/method/LoadRunnerSupportedMethods.java
+++ models-readers/loadrunner-reader/src/main/java/com/neotys/neoload/model/readers/loadrunner/method/LoadRunnerSupportedMethods.java
@@ -1,40 +1,15 @@
 package com.neotys.neoload.model.readers.loadrunner.method;
 
-import java.util.concurrent.ExecutionException;
-
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
+import com.google.common.cache.CacheLoader.InvalidCacheLoadException;
 import com.google.common.cache.LoadingCache;
 import com.neotys.neoload.model.readers.loadrunner.customaction.CustomActionMappingLoader;
 
 public class LoadRunnerSupportedMethods {
 
 	private static final LoadingCache<String, LoadRunnerMethod> SUPPORTED_METHODS = CacheBuilder.newBuilder().build(
-			new CacheLoader<String, LoadRunnerMethod>() {
-				@Override
-				public LoadRunnerMethod load(final String methodName) {
-					if (methodName == null) {
-						throw new InvalidCacheLoadException("Method name is null");
-					}										
-					if (CustomActionMappingLoader.getMapping().get(methodName)!=null) {
-						return new CustomActionMethod();
-					}
-					final String lowerCaseMethodName = methodName.replaceAll("_", "").toLowerCase();
-					if (lowerCaseMethodName.length() < 2) {
-						throw new InvalidCacheLoadException("Method name has less than 2 characters");
-					}
-					final String fullQualifiedMethodName = "com.neotys.neoload.model.readers.loadrunner.method."
-							+ lowerCaseMethodName.substring(0, 1).toUpperCase() + lowerCaseMethodName.substring(1) + "Method";
-					try {
-						return (LoadRunnerMethod) Class.forName(fullQualifiedMethodName).getConstructor().newInstance();
-					} catch (Exception e) {
-					}
-					if(methodName.equals("lr_start_sub_transaction")) return new LrstarttransactionMethod();
-					if(methodName.equals("lr_end_sub_transaction")) return new LrendtransactionMethod();
-					if(methodName.equals("lr_param_sprintf")) return new SprintfMethod();
-					throw new InvalidCacheLoadException("Method not supported");
-				}
-			});
+			CacheLoader.from(LoadRunnerSupportedMethods::internalLoadMethod));
 
 	private LoadRunnerSupportedMethods() {
 		super();
@@ -46,5 +21,28 @@ public class LoadRunnerSupportedMethods {
 		} catch (final Exception e) {
 			return null;
 		}
+	}
+	
+	private static final LoadRunnerMethod internalLoadMethod(final String methodName) throws InvalidCacheLoadException {
+		if (methodName == null) {
+			throw new InvalidCacheLoadException("Method name is null");
+		}										
+		if (CustomActionMappingLoader.getMapping().get(methodName)!=null) {
+			return new CustomActionMethod();
+		}
+		final String lowerCaseMethodName = methodName.replaceAll("_", "").toLowerCase();
+		if (lowerCaseMethodName.length() < 2) {
+			throw new InvalidCacheLoadException("Method name has less than 2 characters");
+		}
+		final String fullQualifiedMethodName = "com.neotys.neoload.model.readers.loadrunner.method."
+				+ lowerCaseMethodName.substring(0, 1).toUpperCase() + lowerCaseMethodName.substring(1) + "Method";
+		try {
+			return (LoadRunnerMethod) Class.forName(fullQualifiedMethodName).getConstructor().newInstance();
+		} catch (Exception e) {
+		}
+		if("lr_start_sub_transaction".equals(methodName)) return new LrstarttransactionMethod();
+		if("lr_end_sub_transaction".equals(methodName)) return new LrendtransactionMethod();
+		if("lr_param_sprintf".equals(methodName)) return new SprintfMethod();
+		throw new InvalidCacheLoadException("Method not supported");
 	}
 }
