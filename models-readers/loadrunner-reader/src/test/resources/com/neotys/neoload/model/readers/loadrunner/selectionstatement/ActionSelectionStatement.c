5e88c3dea77a5a0e305305bf84376e44cfa74fd7
Sébastien Richert <sebastien.richert@neotys.com>
2018-09-27 21:54:35 +0200

    JUnit reader selection statement

---
+++ models-readers/loadrunner-reader/src/test/resources/com/neotys/neoload/model/readers/loadrunner/selectionstatement/ActionSelectionStatement.c
@@ -0,0 +1,14 @@
+Action()
+{
+
+	if (sapgui_is_object_available("wnd[1]")){
+		sapgui_set_ok_code("1");		
+		sapgui_set_ok_code("2");		
+	} else {
+		sapgui_set_ok_code("3");
+		if (sapgui_is_object_available("wnd[1]")){
+			sapgui_set_ok_code("4");
+		}		
+	}	
+	return 0;
+}
