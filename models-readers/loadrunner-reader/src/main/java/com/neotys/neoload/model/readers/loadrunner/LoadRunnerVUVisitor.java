bd41e1f26df68bcb2a25b142c2d50db6551131bf
Sébastien Richert <sebastien.richert@neotys.com>
2018-09-27 20:41:02 +0200

    New visitors for selection statement

--- models-readers/loadrunner-reader/src/main/java/com/neotys/neoload/model/readers/loadrunner/LoadRunnerVUVisitor.java
+++ models-readers/loadrunner-reader/src/main/java/com/neotys/neoload/model/readers/loadrunner/LoadRunnerVUVisitor.java
@@ -14,23 +14,14 @@ import com.neotys.neoload.model.function.Function;
 import com.neotys.neoload.model.listener.EventListener;
 import com.neotys.neoload.model.parsers.CPP14BaseVisitor;
 import com.neotys.neoload.model.parsers.CPP14Parser;
-import com.neotys.neoload.model.parsers.CPP14Parser.ConditionContext;
 import com.neotys.neoload.model.parsers.CPP14Parser.MethodcallContext;
 import com.neotys.neoload.model.parsers.CPP14Parser.SelectionstatementContext;
-import com.neotys.neoload.model.parsers.CPP14Parser.StatementContext;
 import com.neotys.neoload.model.readers.loadrunner.customaction.ImmutableMappingMethod;
 import com.neotys.neoload.model.readers.loadrunner.method.LoadRunnerMethod;
-import com.neotys.neoload.model.repository.Condition;
-import com.neotys.neoload.model.repository.Conditions;
-import com.neotys.neoload.model.repository.Container;
-import com.neotys.neoload.model.repository.CustomAction;
-import com.neotys.neoload.model.repository.CustomActionParameter;
+import com.neotys.neoload.model.readers.loadrunner.selectionstatement.SelectionStatementVisitor;
 import com.neotys.neoload.model.repository.EvalString;
 import com.neotys.neoload.model.repository.Header;
-import com.neotys.neoload.model.repository.ImmutableCondition;
-import com.neotys.neoload.model.repository.ImmutableConditions;
 import com.neotys.neoload.model.repository.ImmutableContainer;
-import com.neotys.neoload.model.repository.ImmutableIfThenElse;
 import com.neotys.neoload.model.repository.Page;
 import com.neotys.neoload.model.repository.Request;
 import com.neotys.neoload.model.repository.Validator;
@@ -90,86 +81,9 @@ public class LoadRunnerVUVisitor extends CPP14BaseVisitor<List<Element>> {
 
 	@Override
 	public List<Element> visitSelectionstatement(final SelectionstatementContext selectionstatementContext) {
-		
-		final String methodName = selectionstatementContext.getChild(0).getText();
-		if(!"if".equals(methodName)){
-			return super.visitSelectionstatement(selectionstatementContext);
-		}
-		final ImmutableIfThenElse.Builder ifThenElseBuilder = ImmutableIfThenElse.builder().name(methodName);
-		
-		final Element condition = selectionstatementContext.getChild(2).accept(new ConditionContextVisitor());
-		final ImmutableConditions.Builder conditionsBuilder = ImmutableConditions.builder();
-		if(condition instanceof CustomAction){
-			conditionsBuilder.addConditions(ImmutableCondition.builder()
-					.operand1(getVariableName((CustomAction)condition))
-					.operator(Condition.Operator.EQUALS)
-					.operand2("true")
-					.build())
-				.matchType(Conditions.MatchType.ANY)
-				.build();			
-		}	
-		ifThenElseBuilder.conditions(conditionsBuilder.build());
-		final List<Element> thenElements = selectionstatementContext.getChild(4).accept(new StatementContextVisitor("Then"));
-		final Container thenContainer = ImmutableContainer.builder().name("Then").addAllChilds(thenElements).build();
-		ifThenElseBuilder.then(thenContainer);
-		final List<Element> elseElements = new ArrayList<>();
-		if(selectionstatementContext.getChildCount() > 6){
-			elseElements.addAll(selectionstatementContext.getChild(6).accept(new StatementContextVisitor("Else")));			
-		}		
-		final Container elseContainer = ImmutableContainer.builder().name("Else").addAllChilds(elseElements).build();
-		ifThenElseBuilder.getElse(elseContainer);		
-		addInCurrentContainer(ifThenElseBuilder.build());
-		return Collections.emptyList();		
+		return ImmutableList.of(selectionstatementContext.accept((new SelectionStatementVisitor(this))));		
 	}
 	
-	private class ConditionContextVisitor extends CPP14BaseVisitor<Element> {
-
-		@Override
-		public Element visitCondition(final ConditionContext ctx) {
-			final Element element = ctx.expression().accept(this);
-			return element;
-		}
-		
-		@Override
-		public Element visitMethodcall(MethodcallContext ctx) {
-			final Element element = LoadRunnerVUVisitor.this.visitMethodcall(ctx).get(0);
-			return element;
-		}
-	}
-	
-	private class StatementContextVisitor extends CPP14BaseVisitor<List<Element>> {
-		
-		private final ImmutableContainer.Builder currentContainer;
-		
-		public StatementContextVisitor(final String name){
-			super();
-			this.currentContainer = ImmutableContainer.builder().name(name);
-		}
-		
-		@Override
-		public List<Element> visitStatement(StatementContext ctx) {
-			super.visitStatement(ctx);
-			return currentContainer.build().getChilds();
-		}
-				
-		@Override
-		public List<Element> visitMethodcall(MethodcallContext ctx) {			 
-			currentContainers.add(currentContainer);
-			LoadRunnerVUVisitor.this.visitMethodcall(ctx);
-			currentContainers.remove(currentContainers.size()-1);
-			return Collections.emptyList();
-		}
-	}
-		
-	private static String getVariableName(final CustomAction customAction) {
-		for(final CustomActionParameter parameter : customAction.getParameters()){
-			if("variable".equals(parameter.getName())){
-				return parameter.getValue();
-			}
-		}
-		return "";
-	}
-
 	@Override
 	protected List<Element> aggregateResult(final List<Element> aggregate, final List<Element> nextResult) {
 		return ImmutableList.of(currentContainers.get(0).build());
---
+++ models-readers/loadrunner-reader/src/main/java/com/neotys/neoload/model/readers/loadrunner/selectionstatement/ConditionContextVisitor.java
@@ -0,0 +1,26 @@
+package com.neotys.neoload.model.readers.loadrunner.selectionstatement;
+
+import com.neotys.neoload.model.core.Element;
+import com.neotys.neoload.model.parsers.CPP14BaseVisitor;
+import com.neotys.neoload.model.parsers.CPP14Parser.ConditionContext;
+import com.neotys.neoload.model.parsers.CPP14Parser.MethodcallContext;
+import com.neotys.neoload.model.readers.loadrunner.LoadRunnerVUVisitor;
+
+public class ConditionContextVisitor extends CPP14BaseVisitor<Element> {
+	
+	private final LoadRunnerVUVisitor visitor;
+	
+	public ConditionContextVisitor(final LoadRunnerVUVisitor visitor) {
+		this.visitor = visitor;
+	}
+
+	@Override
+	public Element visitCondition(final ConditionContext ctx) {
+		return ctx.expression().accept(this);
+	}
+	
+	@Override
+	public Element visitMethodcall(MethodcallContext ctx) {
+		return visitor.visitMethodcall(ctx).get(0);
+	}
+}
