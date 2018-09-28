64ebab9598a98ace7af10536202b8cc2a1f2a285
Sébastien Richert <sebastien.richert@neotys.com>
2018-09-28 22:11:59 +0200

    Manage empty statement

--- models-readers/loadrunner-reader/src/test/java/com/neotys/neoload/model/readers/loadrunner/selectionstatement/SelectionStatementVisitorTest.java
+++ models-readers/loadrunner-reader/src/test/java/com/neotys/neoload/model/readers/loadrunner/selectionstatement/SelectionStatementVisitorTest.java
@@ -27,7 +27,7 @@ public class SelectionStatementVisitorTest {
         try(InputStream targetStream = this.getClass().getResourceAsStream("ActionSelectionStatement.c")) {
             Container container = reader.parseCppFile("{", "}", targetStream, "MyContainer", Charsets.UTF_8);
             assertThat(container).isNotNull();
-            assertThat(container.getChilds().size()).isEqualTo(2);
+            assertThat(container.getChilds().size()).isEqualTo(4);
             final Element isObjectAvailable = container.getChilds().get(0);
             assertThat(isObjectAvailable.getName()).isEqualTo("isObjectAvailable");
             final IfThenElse ifThenElse1 = (IfThenElse) container.getChilds().get(1);
@@ -74,6 +74,13 @@ public class SelectionStatementVisitorTest {
             final Container elseContainer3 = ifThenElse2.getElse();
             assertThat(elseContainer3.getName()).isEqualTo("Else");
             assertThat(elseContainer3.getChilds().size()).isEqualTo(0);
+            final IfThenElse ifThenElse4 = (IfThenElse) container.getChilds().get(3);
+            final Container thenContainer4 = ifThenElse4.getThen();
+            assertThat(thenContainer4.getName()).isEqualTo("Then");   
+            assertThat(thenContainer4.getChilds().size()).isEqualTo(0);                        
+            final Container elseContainer4 = ifThenElse4.getElse();
+            assertThat(elseContainer4.getName()).isEqualTo("Else");
+            assertThat(elseContainer4.getChilds().size()).isEqualTo(0);
         }catch(IOException e) {
             fail("Error reading test stream", e);
         }
