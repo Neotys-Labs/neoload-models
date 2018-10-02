3f6b6dea26b1616912142c856e7b3e8b73457b21
Claude CARPENTIER <claude.carpentier@neotys.com>
2018-10-02 17:41:49 +0200

    Fix some sonar issues

--- neoload-project/src/main/java/com/neotys/neoload/model/serializer/PopulationPolicySerializer.java
+++ neoload-project/src/main/java/com/neotys/neoload/model/serializer/PopulationPolicySerializer.java
@@ -7,7 +7,6 @@ import static com.neotys.neoload.model.serializer.PopulationPolicyConstants.FILE
 
 import java.io.IOException;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
