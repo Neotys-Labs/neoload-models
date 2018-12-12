499932f4b7777510eef26cd455430fbca5588d2e
mlasram <mohamed.lasram@neotys.com>
2018-12-12 15:33:23 +0100

    fix variable extractor test

--- neoload-project/src/test/java/com/neotys/neoload/model/v3/binding/io/IORequestTest.java
+++ neoload-project/src/test/java/com/neotys/neoload/model/v3/binding/io/IORequestTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertNotNull;
 public class IORequestTest extends AbstractIOElementsTest {
 
 	private static Project getRequestOnlyRequired() {
+
 		final UserPath userPath = UserPath.builder()
 				.name("MyUserPath")
 				.actions(Container.builder()
--- neoload-project/src/test/java/com/neotys/neoload/model/v3/binding/io/IOVariableExtractorTest.java
+++ neoload-project/src/test/java/com/neotys/neoload/model/v3/binding/io/IOVariableExtractorTest.java
@@ -1,37 +1,57 @@
 package com.neotys.neoload.model.v3.binding.io;
 
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
+import com.neotys.neoload.model.v3.project.Project;
+import com.neotys.neoload.model.v3.project.userpath.*;
 import org.junit.Test;
 
-import com.neotys.neoload.model.v3.project.userpath.Request;
+import java.io.IOException;
+import java.util.Optional;
+
+import static com.neotys.neoload.model.v3.project.userpath.VariableExtractor.Decode.URL;
+import static org.junit.Assert.assertNotNull;
 
 
 public class IOVariableExtractorTest extends AbstractIOElementsTest {
 
 	@Test
-	public void readVariableOnlyRequired() throws IOException, URISyntaxException {
-//		final Request expectedRequest = buildRequestContainingVariableExtractor();
-//		assertNotNull(expectedRequest);
-//
-//		read("test-variable-extractor", expectedRequest);
+	public void readVariableOnlyRequired() throws IOException {
+		final Project expectedProject = buildRequestContainingVariableExtractor();
+		assertNotNull(expectedProject);
+
+		read("test-variable-extractor", expectedProject);
 	}
 
-	private Request buildRequestContainingVariableExtractor() {
-//		final VariableExtractor variableExtractor = ImmutableVariableExtractor.builder()
-//				.name("first-match-on-jsonpath")
-//				.jsonPath("$.features[0].type")
-//				.regexp("Fea(.*)")
-//				.matchNumber(1)
-//				.decode(Optional.of(URL))
-//				.extractOnce(true)
-//				.build();
-//		return Request.builder()
-//				.name("MyProject")
-//				.addVariableExtractir(variableExtractor)
-//				.build();
-		return null;
+	private Project buildRequestContainingVariableExtractor() {
+
+		final VariableExtractor variableExtractor = VariableExtractor.builder()
+				.name("first-match-on-jsonpath")
+				.jsonPath("$.features[0].type")
+				.regexp("Fea(.*)")
+				.matchNumber(1)
+				.decode(Optional.of(URL))
+				.extractOnce(true)
+				.build();
+
+		final ImmutableRequest request = Request.builder()
+				.name("request")
+				.url("http://www.neotys.com/select?name:neoload")
+				.addExtractors(variableExtractor)
+				.build();
+
+		final UserPath userPath = UserPath.builder()
+				.name("MyUserPath")
+				.actions(Container.builder()
+						.name("actions")
+						.addSteps(request)
+						.build())
+				.build();
+
+		return Project.builder()
+				.name("MyProject")
+				.addUserPaths(userPath)
+				.build();
+
 	}
+
 }
