7dbd22c6cc194165760cf339a0656195804caced
Claude CARPENTIER <claude.carpentier@neotys.com>
2018-12-17 15:04:00 +0100

    Remove some constants

--- neoload-project/src/main/java/com/neotys/neoload/model/v3/project/userpath/Delay.java
+++ neoload-project/src/main/java/com/neotys/neoload/model/v3/project/userpath/Delay.java
@@ -1,18 +1,10 @@
 package com.neotys.neoload.model.v3.project.userpath;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.immutables.value.Value;
 
-@JsonInclude(value = JsonInclude.Include.NON_EMPTY)
-@JsonPropertyOrder({Delay.DELAY})
-@JsonDeserialize(as = ImmutableDelay.class)
 @Value.Immutable
 @Value.Style(validationMethod = Value.Style.ValidationMethod.NONE)
 public interface Delay extends StepDuration {
-	String DELAY = "delay";
-
 	class Builder extends ImmutableDelay.Builder {}
 	static Builder builder() {
 		return new Builder();
