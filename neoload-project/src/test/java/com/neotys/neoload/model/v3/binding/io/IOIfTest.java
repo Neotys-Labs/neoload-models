aadf94107e59302a36587d5e6635ee316fa54332
Sébastien Richert <sebastien.richert@neotys.com>
2019-03-21 19:26:26 +0100

    #23279 As-code - If-Then-Else - Junit tests

---
+++ neoload-project/src/test/java/com/neotys/neoload/model/v3/binding/io/IOIfTest.java
@@ -0,0 +1,143 @@
+package com.neotys.neoload.model.v3.binding.io;
+
+
+import com.neotys.neoload.model.v3.project.Project;
+import com.neotys.neoload.model.v3.project.userpath.*;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+
+public class IOIfTest extends AbstractIOElementsTest {
+
+	private static Project getIfOnlyRequired() {
+		final UserPath userPath = UserPath.builder()
+				.name("MyUserPath")
+				.actions(Container.builder()
+						.name("actions")
+						.addSteps(If.builder()
+								.conditions(Arrays.asList(Condition
+										.builder()
+										.operand1("operand1")
+										.operator(Condition.Operator.EQUALS)
+										.operand2("operand2")
+										.build()))
+								.then(Container.builder()
+										.addSteps(Delay
+												.builder()
+												.value(String.valueOf(3*60*1000+200)) // "3m 200ms"
+												.build()
+										).build())
+								.build())
+						.build())
+				.build();
+
+		final Project project = Project.builder()
+				.name("MyProject")
+				.addUserPaths(userPath)
+				.build();
+
+		return project;
+	}
+
+	private static Project getIfRequiredAndOptional() {
+		final UserPath userPath = UserPath.builder()
+				.name("MyUserPath")
+				.actions(Container.builder()
+						.name("actions")
+						.addSteps(If.builder()
+								.name("My If-Then-Else")
+								.description("My description")
+								.conditions(getConditions())
+								.match(If.Match.ANY)
+								.then(getThen())
+								.getElse(getElse())
+								.build())
+						.build())
+				.build();
+
+		final Project project = Project.builder()
+				.name("MyProject")
+				.addUserPaths(userPath)
+				.build();
+
+		return project;
+	}
+
+	private static final List<Condition> getConditions(){
+		return Arrays.asList(
+				getCondition("operand1", Condition.Operator.EQUALS, "operand2"),
+				getCondition("0", Condition.Operator.EQUALS, "operand2"),
+				getCondition("operand1", Condition.Operator.NOT_EQUALS, "${parameter}"),
+				getCondition("${parameter}", Condition.Operator.NOT_EQUALS, "10"),
+				getCondition("\"${parameter}\"", Condition.Operator.CONTAINS, "\"contains\""),
+				getCondition("\"operand1\"", Condition.Operator.NOT_CONTAINS, "\"operand2\""),
+				getCondition("operand1", Condition.Operator.STARTS_WITH, "\"==\""),
+				getCondition("operand1", Condition.Operator.NOT_STARTS_WITH, "operand2"),
+				getCondition("operand1", Condition.Operator.ENDS_WITH, "operand2"),
+				getCondition("operand1", Condition.Operator.NOT_ENDS_WITH, "operand2"),
+				getCondition("operand1", Condition.Operator.MATCH_REGEXP, "operand2"),
+				getCondition("operand1", Condition.Operator.NOT_MATCH_REGEXP, "operand2"),
+				getCondition("operand1", Condition.Operator.GREATER, "operand2"),
+				getCondition("operand1", Condition.Operator.GREATER, "operand2"),
+				getCondition("operand1", Condition.Operator.GREATER_EQUAL, "operand2"),
+				getCondition("operand1", Condition.Operator.GREATER_EQUAL, "operand2"),
+				getCondition("operand1", Condition.Operator.LESS, "operand2"),
+				getCondition("operand1", Condition.Operator.LESS, "operand2"),
+				getCondition("operand1", Condition.Operator.LESS_EQUAL, "operand2"),
+				getCondition("operand1", Condition.Operator.LESS_EQUAL, "operand2"),
+				getCondition("operand1", Condition.Operator.EXISTS, "operand2"),
+				getCondition("operand1", Condition.Operator.NOT_EXISTS, "operand2"),
+				getCondition("operand1", Condition.Operator.FIND_REGEXP, "operand2")
+		);
+	}
+
+	private static final Condition getCondition(final String operand1, final Condition.Operator operator, final String operand2){
+		return Condition
+				.builder()
+				.operand1(operand1).operator(operator).operand2(operand2)
+				.build();
+	}
+
+	private static Container getThen() {
+		return Container.builder()
+				.description("My then description")
+				.slaProfile("MySLAProfile")
+				.addSteps(Request
+						.builder()
+						.url("http://www.neotys.com/select")
+						.build()
+				).build();
+	}
+
+	private static Container getElse() {
+		return Container.builder()
+				.description("My else description")
+				.slaProfile("MySLAProfile")
+				.addSteps(Delay
+						.builder()
+						.value("3m 200ms")
+						.build()
+				).build();
+	}
+
+	@Test
+	public void readUserPathsOnlyRequired() throws IOException {
+		final Project expectedProject = getIfOnlyRequired();
+		assertNotNull(expectedProject);
+
+		read("test-if-only-required", expectedProject);
+	}
+
+	@Test
+	public void readUserPathsRequiredAndOptional() throws IOException {
+		final Project expectedProject = getIfRequiredAndOptional();
+		assertNotNull(expectedProject);
+
+		//read("test-if-required-and-optional", expectedProject);
+	}
+}
---
+++ neoload-project/src/test/java/com/neotys/neoload/model/v3/project/userpath/ConditionTest.java
@@ -0,0 +1,62 @@
+package com.neotys.neoload.model.v3.project.userpath;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+
+public class ConditionTest {
+	@Test
+	public void testOperatorConstants() {
+		assertEquals(Condition.Operator.EQUALS, Condition.Operator.of("equals"));
+		assertEquals(Condition.Operator.EQUALS, Condition.Operator.of("=="));
+		assertEquals(Condition.Operator.NOT_EQUALS, Condition.Operator.of("not_equals"));
+		assertEquals(Condition.Operator.NOT_EQUALS, Condition.Operator.of("!="));
+		assertEquals(Condition.Operator.CONTAINS, Condition.Operator.of("contains"));
+		assertEquals(Condition.Operator.NOT_CONTAINS, Condition.Operator.of("not_contains"));
+		assertEquals(Condition.Operator.STARTS_WITH, Condition.Operator.of("starts_with"));
+		assertEquals(Condition.Operator.NOT_STARTS_WITH, Condition.Operator.of("not_starts_with"));
+		assertEquals(Condition.Operator.ENDS_WITH, Condition.Operator.of("ends_with"));
+		assertEquals(Condition.Operator.NOT_ENDS_WITH, Condition.Operator.of("not_ends_with"));
+		assertEquals(Condition.Operator.MATCH_REGEXP, Condition.Operator.of("match_regexp"));
+		assertEquals(Condition.Operator.NOT_MATCH_REGEXP, Condition.Operator.of("not_match_regexp"));
+		assertEquals(Condition.Operator.GREATER, Condition.Operator.of("greater"));
+		assertEquals(Condition.Operator.GREATER, Condition.Operator.of(">"));
+		assertEquals(Condition.Operator.GREATER_EQUAL, Condition.Operator.of("greater_equal"));
+		assertEquals(Condition.Operator.GREATER_EQUAL, Condition.Operator.of(">="));
+		assertEquals(Condition.Operator.LESS, Condition.Operator.of("less"));
+		assertEquals(Condition.Operator.LESS, Condition.Operator.of("<"));
+		assertEquals(Condition.Operator.LESS_EQUAL, Condition.Operator.of("less_equal"));
+		assertEquals(Condition.Operator.LESS_EQUAL, Condition.Operator.of("<="));
+		assertEquals(Condition.Operator.EXISTS, Condition.Operator.of("exists"));
+		assertEquals(Condition.Operator.NOT_EXISTS, Condition.Operator.of("not_exists"));
+		assertEquals(Condition.Operator.FIND_REGEXP, Condition.Operator.of("find_regexp"));
+	}
+
+	@Test
+	public void testExceptionMessage() {
+		final String operatorMustBe = "The operator must be: [==, <=, not_match_regexp, less, starts_with, greater_equal, not_equals, not_exists, contains, ends_with, not_contains, not_ends_with, equals, match_regexp, exists, !=, <, find_regexp, greater, >, less_equal, not_starts_with, >=].";
+		final String operatorNotNullOrEmpty = "The operator must not be null or empty.";
+
+		try{
+			Condition.Operator.of("unexisting operator");
+			fail();
+		} catch(final IllegalArgumentException iae){
+			assertEquals(operatorMustBe, iae.getMessage());
+		}
+		try{
+			Condition.Operator.of("");
+			fail();
+		} catch(final IllegalArgumentException iae){
+			assertEquals(operatorNotNullOrEmpty, iae.getMessage());
+		}
+		try{
+			Condition.Operator.of(null);
+			fail();
+		} catch(final IllegalArgumentException iae){
+			assertEquals(operatorNotNullOrEmpty, iae.getMessage());
+		}
+	}
+}
