7474ec90f70a175facd3cbf1dade7654a0e83f66
Claude CARPENTIER <claude.carpentier@neotys.com>
2019-04-04 12:16:16 +0200

    Fix some junit coverage issues

--- neoload-project/src/test/java/com/neotys/neoload/model/v3/binding/io/ProjectDescriptorTest.java
+++ neoload-project/src/test/java/com/neotys/neoload/model/v3/binding/io/ProjectDescriptorTest.java
@@ -5,14 +5,12 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import com.neotys.neoload.model.v3.project.Project;
-
 
 public class ProjectDescriptorTest {
 	@Test
 	public void name() {
 		assertEquals("includes", ProjectDescriptor.INCLUDES);
 		
-		assertEquals(Project.builder().build(), ProjectDescriptor.DEFAULT_PROJECT);
+		assertEquals(ProjectDescriptor.builder().build().getProject(), ProjectDescriptor.DEFAULT_PROJECT);
 	}
 }
