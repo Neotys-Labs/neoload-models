20e1aa2948aca5add6afae4b791966b0014bf8a6
Alain Origlia <alain.origlia@neotys.com>
2019-08-06 15:35:43 +0200

    Bug fix. Element and Writer must have the same char case.

--- neoload-writer/src/main/java/com/neotys/neoload/model/v3/writers/neoload/userpath/ThinkTimeWriter.java
+++ neoload-writer/src/main/java/com/neotys/neoload/model/v3/writers/neoload/userpath/ThinkTimeWriter.java
@@ -5,18 +5,18 @@ import com.neotys.neoload.model.v3.writers.neoload.ElementWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-public class ThinktimeWriter extends ElementWriter {
+public class ThinkTimeWriter extends ElementWriter {
 
     public static final String XML_TAG_NAME = "delay-action";
     public static final String XML_DURATION_ATT = "duration";
     public static final String XML_ISTHINKTIME_ATT = "isThinkTime";
 
-    public ThinktimeWriter(ThinkTime thinktime) {
+    public ThinkTimeWriter(ThinkTime thinktime) {
         super(thinktime);
     }
 
-    public static ThinktimeWriter of(final ThinkTime thinktime) {
-        return new ThinktimeWriter(thinktime);
+    public static ThinkTimeWriter of(final ThinkTime thinktime) {
+        return new ThinkTimeWriter(thinktime);
     }
 
     @Override
