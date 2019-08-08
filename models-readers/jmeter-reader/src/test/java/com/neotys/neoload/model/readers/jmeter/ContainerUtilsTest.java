8a2b63a6ab974f65103636a945e1339b80756a12
ThomasMartinezNeotys <51602865+ThomasMartinezNeotys@users.noreply.github.com>
2019-08-08 16:18:27 +0200

    Implements test ContainerUtils

--- models-readers/jmeter-reader/src/test/java/com/neotys/neoload/model/readers/jmeter/ContainerUtilsTest.java
+++ models-readers/jmeter-reader/src/test/java/com/neotys/neoload/model/readers/jmeter/ContainerUtilsTest.java
@@ -1,8 +1,29 @@
 package com.neotys.neoload.model.readers.jmeter;
 
-import static org.junit.Assert.*;
+import com.neotys.neoload.model.v3.project.variable.ConstantVariable;
+import com.neotys.neoload.model.v3.project.variable.Variable;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class ContainerUtilsTest {
-    //TODO
+
+    @Test
+    public void testGetterValue(){
+        new ContainerUtils();
+        ContainerUtils.addKeyValue("Stagiaire","Thomas");
+        assertEquals(ContainerUtils.getValue("${Stagiaire}"),"Thomas");
+
+        Variable constantVariable = ConstantVariable.builder().build();
+        List<Variable> expected = new ArrayList<>();
+        expected.add(constantVariable);
+        ContainerUtils.addVariable(expected);
+        List<Variable> result = ContainerUtils.getVariableContainer();
+        assertEquals(result,expected);
+
+    }
 
 }
