3e27e918a069defd2282091fc5b0964f0aa4cf85
Alain Origlia <alain.origlia@neotys.com>
2019-08-27 17:45:02 +0200

    Fix: Case of NPE when no default case in jmeter switch controller

--- neoload-writer/src/main/java/com/neotys/neoload/model/v3/writers/neoload/userpath/SwitchWriter.java
+++ neoload-writer/src/main/java/com/neotys/neoload/model/v3/writers/neoload/userpath/SwitchWriter.java
@@ -1,13 +1,12 @@
 package com.neotys.neoload.model.v3.writers.neoload.userpath;
 
 import com.neotys.neoload.model.v3.project.userpath.Case;
+import com.neotys.neoload.model.v3.project.userpath.Container;
 import com.neotys.neoload.model.v3.project.userpath.Switch;
 import com.neotys.neoload.model.v3.writers.neoload.ElementWriter;
 import com.neotys.neoload.model.v3.writers.neoload.WriterUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-
-import java.util.Optional;
 
 public class SwitchWriter extends ElementWriter {
 
@@ -37,9 +36,10 @@ public class SwitchWriter extends ElementWriter {
                     elt -> WriterUtils.<ElementWriter>getWriterFor(elt).writeXML(document, currentElement, outputFolder)
             );
         }
-        ContainerWriter.of(aSwitch.getDefault(), "default-statement").writeXML(document, switchElement, outputFolder);
-
-
-
+        if (aSwitch.getDefault() != null) {
+            ContainerWriter.of(aSwitch.getDefault(), "default-statement").writeXML(document, switchElement, outputFolder);
+        } else {
+            ContainerWriter.of((new Container.Builder()).name("Default").build(), "default-statement").writeXML(document, switchElement, outputFolder);
+        }
     }
 }
