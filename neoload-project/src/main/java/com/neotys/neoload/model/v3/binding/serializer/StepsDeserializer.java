d1e3ae43a24979cee15b9278a706f1aff7ab9818
Claude CARPENTIER <claude.carpentier@neotys.com>
2020-08-06 16:14:33 +0200

    Fix sonar issues

--- neoload-project/src/main/java/com/neotys/neoload/model/v3/binding/serializer/StepsDeserializer.java
+++ neoload-project/src/main/java/com/neotys/neoload/model/v3/binding/serializer/StepsDeserializer.java
@@ -15,12 +15,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.google.common.collect.ImmutableMap;
 import com.neotys.neoload.model.v3.project.userpath.Container;
 import com.neotys.neoload.model.v3.project.userpath.Delay;
 import com.neotys.neoload.model.v3.project.userpath.If;
@@ -34,59 +36,58 @@ import com.neotys.neoload.model.v3.project.userpath.While;
 
 public class StepsDeserializer extends StdDeserializer<List<Step>> {
     private static final long serialVersionUID = -5696608939252369276L;
-
+    
+    private static final Map<String, Class<? extends Step>> STEPS;
+    static {
+    	final ImmutableMap.Builder<String, Class<? extends Step>> builder = new ImmutableMap.Builder<String, Class<? extends Step>>();
+    	builder.put(TRANSACTION, Container.class);
+    	builder.put(REQUEST, Request.class);
+    	builder.put(JAVASCRIPT, JavaScript.class);
+    	builder.put(IF, If.class);
+    	builder.put(LOOP, Loop.class);
+    	builder.put(WHILE, While.class);
+    	builder.put(SWITCH, Switch.class);
+    	STEPS = builder.build();
+    }
+    
     public StepsDeserializer() {
         super(List.class);
     }
 
     @Override
     public List<Step> deserialize(final JsonParser jsonParser, final DeserializationContext deserializationContext) throws IOException {
-        final List<Step> actions = new ArrayList<>();
+        final List<Step> steps = new ArrayList<>();
 
         final ObjectCodec codec = jsonParser.getCodec();
         final JsonNode jsonNode = codec.readTree(jsonParser);
 
         final Iterator<JsonNode> iterator = jsonNode.elements();
         while (iterator.hasNext()) {
-            final JsonNode actionNode = iterator.next();
-
-            Step action = null;
-            if (actionNode.has(TRANSACTION)) {
-                final JsonNode transactionNode = actionNode.get(TRANSACTION);
-                action = codec.treeToValue(transactionNode, Container.class);
-            } else if (actionNode.has(REQUEST)) {
-                final JsonNode requestNode = actionNode.get(REQUEST);
-                action = codec.treeToValue(requestNode, Request.class);
-            } else if (actionNode.has(DELAY)) {
-                final String delayValue = actionNode.get(DELAY).asText();
+            final JsonNode stepNode = iterator.next();
+           
+            Step step = null;
+            if (stepNode.has(DELAY)) {
+                final String delayValue = stepNode.get(DELAY).asText();
                 final String delay = STRING_TO_TIME_DURATION_IN_MS_OR_IN_VARIABLE.convert(delayValue);
-                action = Delay.builder().value(String.valueOf(delay)).build();
-            } else if (actionNode.has(THINK_TIME)) {
-                final String thinkTimeValue = actionNode.get(THINK_TIME).asText();
+                step = Delay.builder().value(String.valueOf(delay)).build();
+            } else if (stepNode.has(THINK_TIME)) {
+                final String thinkTimeValue = stepNode.get(THINK_TIME).asText();
                 final String thinkTime = STRING_TO_TIME_DURATION_IN_MS_OR_IN_VARIABLE.convert(thinkTimeValue);
-                action = ThinkTime.builder().value(String.valueOf(thinkTime)).build();
-            } else if (actionNode.has(JAVASCRIPT)) {
-                final JsonNode javascriptNode = actionNode.get(JAVASCRIPT);
-                action = codec.treeToValue(javascriptNode, JavaScript.class);
-            } else if (actionNode.has(IF)) {
-                final JsonNode ifNode = actionNode.get(IF);
-                action = codec.treeToValue(ifNode, If.class);
-            } else if (actionNode.has(LOOP)) {
-                final JsonNode loopNode = actionNode.get(LOOP);
-                action = codec.treeToValue(loopNode, Loop.class);
-            } else if (actionNode.has(WHILE)) {
-                final JsonNode whileNode = actionNode.get(WHILE);
-                action = codec.treeToValue(whileNode, While.class);
-            } else if (actionNode.has(SWITCH)){
-                final JsonNode switchNode = actionNode.get(SWITCH);
-                action = codec.treeToValue(switchNode,Switch.class);
+                step = ThinkTime.builder().value(String.valueOf(thinkTime)).build();
+            } else {
+            	final String stepName = stepNode.fieldNames().next();
+            	final Class<? extends Step> stepClass = STEPS.get(stepName);
+            	if (stepClass != null) {
+            		final JsonNode stepValue = stepNode.get(stepName);
+            		step = codec.treeToValue(stepValue, stepClass);
+            	}
             }
 
-            if (action != null) {
-                actions.add(action);
+            if (step != null) {
+                steps.add(step);
             }
         }
 
-        return actions;
+        return steps;
     }
 }
--- neoload-project/src/main/java/com/neotys/neoload/model/v3/binding/serializer/StepsSerializer.java
+++ neoload-project/src/main/java/com/neotys/neoload/model/v3/binding/serializer/StepsSerializer.java
@@ -2,22 +2,49 @@ package com.neotys.neoload.model.v3.binding.serializer;
 
 import static com.neotys.neoload.model.v3.binding.converter.TimeDurationInMsOrInVariableToStringConverter.TIME_DURATION_IN_MS_OR_IN_VARIABLE_TO_STRING;
 import static com.neotys.neoload.model.v3.binding.serializer.StepsConstants.DELAY;
+import static com.neotys.neoload.model.v3.binding.serializer.StepsConstants.IF;
+import static com.neotys.neoload.model.v3.binding.serializer.StepsConstants.JAVASCRIPT;
+import static com.neotys.neoload.model.v3.binding.serializer.StepsConstants.LOOP;
+import static com.neotys.neoload.model.v3.binding.serializer.StepsConstants.REQUEST;
+import static com.neotys.neoload.model.v3.binding.serializer.StepsConstants.SWITCH;
 import static com.neotys.neoload.model.v3.binding.serializer.StepsConstants.THINK_TIME;
 import static com.neotys.neoload.model.v3.binding.serializer.StepsConstants.TRANSACTION;
+import static com.neotys.neoload.model.v3.binding.serializer.StepsConstants.WHILE;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.neotys.neoload.model.v3.project.userpath.Container;
+import com.google.common.collect.ImmutableMap;
 import com.neotys.neoload.model.v3.project.userpath.Delay;
+import com.neotys.neoload.model.v3.project.userpath.ImmutableContainer;
+import com.neotys.neoload.model.v3.project.userpath.ImmutableIf;
+import com.neotys.neoload.model.v3.project.userpath.ImmutableJavaScript;
+import com.neotys.neoload.model.v3.project.userpath.ImmutableLoop;
+import com.neotys.neoload.model.v3.project.userpath.ImmutableRequest;
+import com.neotys.neoload.model.v3.project.userpath.ImmutableSwitch;
+import com.neotys.neoload.model.v3.project.userpath.ImmutableWhile;
 import com.neotys.neoload.model.v3.project.userpath.Step;
 import com.neotys.neoload.model.v3.project.userpath.ThinkTime;
 
 public class StepsSerializer extends StdSerializer<List<Step>> {
     private static final long serialVersionUID = -4569870233567503685L;
+
+    private static final Map<Class<? extends Step>, String> STEPS;
+    static {
+    	final ImmutableMap.Builder<Class<? extends Step>, String> builder = new ImmutableMap.Builder<Class<? extends Step>, String>();
+    	builder.put(ImmutableContainer.class, TRANSACTION);
+    	builder.put(ImmutableRequest.class, REQUEST);
+    	builder.put(ImmutableJavaScript.class, JAVASCRIPT);
+    	builder.put(ImmutableIf.class, IF);
+    	builder.put(ImmutableLoop.class, LOOP);
+    	builder.put(ImmutableWhile.class, WHILE);
+    	builder.put(ImmutableSwitch.class, SWITCH);
+    	STEPS = builder.build();
+    }
 
     public StepsSerializer() {
         super(List.class, false);
@@ -28,12 +55,7 @@ public class StepsSerializer extends StdSerializer<List<Step>> {
 		generator.writeStartArray();
 				
 		for (final Step step : steps) {
-			if (step instanceof Container) {
-				generator.writeStartObject();
-				generator.writeObjectField(TRANSACTION, (Container)step);
-				generator.writeEndObject();
-			}
-			else if (step instanceof Delay) {
+			if (step instanceof Delay) {
 				generator.writeStartObject();
 				generator.writeStringField(DELAY, TIME_DURATION_IN_MS_OR_IN_VARIABLE_TO_STRING.convert(((Delay)step).getValue()));
 				generator.writeEndObject();
@@ -42,6 +64,14 @@ public class StepsSerializer extends StdSerializer<List<Step>> {
 				generator.writeStartObject();
 				generator.writeStringField(THINK_TIME, TIME_DURATION_IN_MS_OR_IN_VARIABLE_TO_STRING.convert(((ThinkTime)step).getValue()));
 				generator.writeEndObject();
+			}
+			else {
+				final String stepName = STEPS.get(step.getClass());
+				if (stepName != null) {
+					generator.writeStartObject();
+					generator.writeObjectField(stepName, step);
+					generator.writeEndObject();
+				} 
 			}
 		}
 		
