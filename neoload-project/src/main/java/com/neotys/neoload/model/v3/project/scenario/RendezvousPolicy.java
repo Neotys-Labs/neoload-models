5687749a91af3f880c0418e12d17ba2bd5d5267e
mlasram <mohamed.lasram@neotys.com>
2020-08-31 09:13:19 +0200

    add serializer to RendezvousPolicy

--- neoload-project/src/main/java/com/neotys/neoload/model/v3/project/scenario/RendezvousPolicy.java
+++ neoload-project/src/main/java/com/neotys/neoload/model/v3/project/scenario/RendezvousPolicy.java
@@ -2,11 +2,15 @@ package com.neotys.neoload.model.v3.project.scenario;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 import javax.validation.Valid;
 
 @JsonInclude(value= JsonInclude.Include.NON_EMPTY)
+@JsonSerialize(as = ImmutableRendezvousPolicy.class)
+@JsonDeserialize(as = ImmutableRendezvousPolicy.class)
 @Value.Immutable
 @Value.Style(validationMethod = Value.Style.ValidationMethod.NONE)
 public interface RendezvousPolicy {
