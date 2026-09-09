31c03944b0def4b2725c858da38f9864d8195273
Wilson Le Faou <w.le-faou@tricentis.com>
2026-09-09 17:25:45 +0200

    LOAD-36918: add bodybinary property to JSON schema for request

--- neoload-project/src/test/java/com/neotys/neoload/model/v3/project/userpath/RequestTest.java
+++ neoload-project/src/test/java/com/neotys/neoload/model/v3/project/userpath/RequestTest.java
@@ -17,6 +17,7 @@ public class RequestTest {
 		assertEquals("method", Request.METHOD);
 		assertEquals("headers", Request.HEADERS);
 		assertEquals("body", Request.BODY);
+		assertEquals("bodybinary", Request.BODYBINARY);
 		assertEquals("extractors", Request.EXTRACTORS);
 		assertEquals("assertions", Request.ASSERTIONS);
 		
